a5dfc7e0d511406ca708b242d010b54d13f9916c
somu16 <somu16041997@gmail.com>
2020-05-11 14:47:42 +0530

    Refractored the code

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -8,6 +8,9 @@ class CollectionsController < ApplicationController
     if collection[:reference_id].present? && collection[:collected_amount].present?
       Collection.create!(reference_id: collection[:reference_id], collected_amount: collection[:collected_amount], collection_date: collection[:collection_date])
     end
-    redirect_to '/'
+
+    invoice_id = Invoice.find_by(reference_id: collection['reference_id'])&.id
+
+    redirect_to controller: 'invoices' , action: "show", id: invoice_id
   end
 end
